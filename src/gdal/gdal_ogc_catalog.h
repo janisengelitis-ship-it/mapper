0ff70e478551849f036b2d5ca1b773f2a26df355
Janis Engelitis <janisengelitis@gmail.com>
2026-09-06 09:21:59 +0300

    Stage WMS/WMTS upstream files

---
+++ src/gdal/gdal_ogc_catalog.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2026 The OpenOrienteering developers
+ *
+ * This file is part of OpenOrienteering.
+ * OpenOrienteering is free software under GNU GPL version 3 or later.
+ */
+
+#ifndef OPENORIENTEERING_GDAL_OGC_CATALOG_H
+#define OPENORIENTEERING_GDAL_OGC_CATALOG_H
+
+#include <QCoreApplication>
+#include <QString>
+#include <QVector>
+
+#include "gdal/gdal_ogc_connection.h"
+
+namespace OpenOrienteering {
+
+struct GdalOgcSource
+{
+	QString dataset_name;
+	QString description;
+	QString title;
+	QString layer_name;
+	QString crs;
+	QString format;
+	QString style;
+	QString tile_matrix_set;
+};
+
+struct GdalOgcCatalogResult
+{
+	GdalOgcConnection::ServiceType detected_type = GdalOgcConnection::ServiceType::Auto;
+	QVector<GdalOgcSource> sources;
+	QString error;
+
+	bool ok() const { return !sources.isEmpty(); }
+};
+
+class GdalOgcCatalog
+{
+	Q_DECLARE_TR_FUNCTIONS(OpenOrienteering::GdalOgcCatalog)
+
+public:
+	static GdalOgcCatalogResult discover(const GdalOgcConnection& connection);
+	static GdalOgcSource describeSource(const GdalOgcConnection& connection,
+	                                   const QString& dataset_name,
+	                                   const QString& description);
+};
+
+}  // namespace OpenOrienteering
+
+#endif
